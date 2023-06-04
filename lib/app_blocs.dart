ae77c53707c1817f0eb3272f38dc83c3761b07e5
idham16ip <idham16iramap@gmai.com>
2023-06-04 17:51:52 +0700

    Practice BLoC management state

---
+++ lib/app_blocs.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_fl_bloc/app_events.dart';
+import 'package:project_fl_bloc/app_states.dart';
+
+class AppBlocs extends Bloc<AppEvent, AppStates> {
+  AppBlocs() : super(InitStates()) {
+    on<Increment>((event, emit) {
+      emit(AppStates(counter: state.counter + 1));
+      print(state.counter);
+    });
+
+    //
+    on<Decrement>((event, emit) {
+      emit(AppStates(counter: state.counter - 1));
+      print(state.counter);
+    });
+  }
+}
